4152ad23d0db3110332faae736e91b39bfe20ff8
ahmed-bs <medamed777@gmail.com>
2022-12-25 13:33:21 +0100

    all table in one and adding a new struct

--- projmain.cpp
+++ projmain.cpp
@@ -1,197 +1,205 @@
 #include<stdio.h>
 #include<conio.h>
 #include <stdlib.h>
-//this function to remplir a table with 0;
-InitBoard(int tab[9][9]){
+#include<windows.h>
+
+//this is a new type named player
+struct {
+int col;
+int row;
+int bars_numbers;
+int id;
+int turn;
+}typedef player;
+
+
+
+//this function to remplir a table with 0 ,3,and 4;
+//0 is the places where the pawn can move to
+//3 is the places with bars
+//also the 4 is the places with bars but the pawns can t pass throught it
+InitBoard(int tab[19][19]){
 int i,j;
- for (i=0;i<9;i++){
-       for (j=0;j<9;j++){
-          tab[i][j]=0;
+ for (i=0;i<19;i++){
+       for (j=0;j<19;j++){
+            tab[i][j]=0;
+//the pair places(this for rows only) will have a bar in them
+            if(i%2==0){
+            tab[i][j]=3;
+            }
+//the pair places(this for colones only) will have a bar in them
+            if(j%2==0){
+            tab[i][j]=3;
+            }
+//this is to fill the cadre with 4 so the pawns can t skip it
+            if(j==0 || j==18){
+            tab[i][j]=4;
+            }
+            if(i==0 || i==18){
+            tab[i][j]=4;
+            }
         }
     }
 }
-//this function is to move pawns
-MovePawns(int tab[9][9],int pawnNum,int* i,int* k){
-int key;
-if (kbhit()) {
-    key=getch();
-    if (key == 224) {
-        do {
-            key=getch();
-        } while(key==224);
-        switch (key) {
-            case 72:
-                tab[*i][*k]=0;
-                tab[*i-1][*k]=pawnNum;
-                *i=*i-1;
-                //printf("up");
-                break;
-            case 75:
-                tab[*i][*k]=0;
-                tab[*i][*k-1]=pawnNum;
-                *k=*k-1;
-                //printf("left");
-                break;
-            case 77:
-                tab[*i][*k]=0;
-                tab[*i][*k+1]=pawnNum;
-                *k=*k+1;
-                //printf("right");
-                break;
-            case 80:
-                tab[*i][*k]=0;
-                tab[*i+1][*k]=pawnNum;
-                *i=*i+1;
-                //printf("down");
-                break;
-        }
-    }
-}
-system("clear");
-}
+//like it s name said ,it will affiche
+AfficheBoard(int tab[19][19]){
+int i,j;
+ for (i=0;i<19;i++){
+       for (j=0;j<19;j++){
 
-//this function is affiche the inside of the table
-AffichePawn(int tab[9][9],int i,int j){
-    if(tab[i][j]==0){
-        printf("%c%c",176,186);
-    }else if(tab[i][j]==1){
-        printf("%c%c",1,186);
-    }else if(tab[i][j]==2){
-        printf("%c%c",2,186);
-    }
-}
-
-
-
-//this function is to fill the table with ascci caracter that will make the border rows of the main table
-InitBorderRows(int tab2[19][19]){
-    int i,j;
-    for (i=0;i<19;i++){
-        for (j=0;j<19;j++){
-            if(i==0 && j==0){
-                tab2[i][j] = 201;
+            if(tab[i][j]==0){
+                //if the case is empty
+                printf(" ");
+            }else if(tab[i][j]==1){
+                //if player 1 is in this case
+                printf("%c",1);
+            }else if(tab[i][j]==2){
+                //if player 2 is in this case
+                printf("%c",2);
+            }else if(i==0 && j==0){
+                //╔
+                printf("%c",201);
             }else if(i==0 && j==18){
-                tab2[i][j] = 187;
+                //╗
+                printf("%c",187);
             }else if(i==0 && j%2==0){
-                tab2[i][j] = 203;
-            }else if(i==0 && j%2==1){
-                tab2[i][j] = 205;
-            }else if(j==0){
-                tab2[i][j] = 204;
-            }else if(j==18){
-                tab2[i][j] = 185;
-            }else if(j%2==0){
-                tab2[i][j]=206;
-            }else{tab2[i][j]=205;
+                //╦
+                printf("%c",203);
+            }else if(i==18 && j==18){
+                //╝
+                printf("%c",188);
+            }else if(i==18 && j==0){
+                //╚
+                printf("%c",200);
+            }else if(i==18 && j%2==0){
+                //╩
+                printf("%c",202);
+            }else if(j==0 && i%2==0){
+                //╠
+                printf("%c",204);
+            }else if(j==18 && i%2==0){
+                // ╣
+                printf("%c",185);
+            }else if(i%2==1 && (tab[i][j] == 3 ||tab[i][j] == 4 )){
+                // ║
+                printf("%c",186);
+            }else if(i%2==0 && (tab[i][j] == 3 ||tab[i][j] == 4 ) && j%2==0){
+                //╬
+                printf("%c",206);
+            }else if(i%2==0){
+                //═
+                printf("%c",205);
             }
         }
-    }
-}
-//this is to initialize the borders colomns
-InitBorderColomns(int tab2[10][10]){
-    int i,j;
-    for (i=0;i<10;i++){
-        for (j=0;j<10;j++){
-            tab2[i][j]=186;
-        }
-    }
-}
-
-//this function is to print the skullet of the table
-AfficheBoard(int tab[9][9]){
-//var  function
-int i,j,k;
-//this to init the table of rows
-int borderRows[19][19];
-//this to init the table of Colomns
-int borderColomns[10][10];
-//this is a function ,go back look for it to see what is does
-InitBorderRows(borderRows);
-InitBorderColomns(borderColomns);
-//this print is to make space for the indexation so it will align with table rows
-printf("  ");
-//this for is to make the indexation of the top row
-    for (i=0;i<9;i++){
-        printf("%d ",i+1);
-    }
-//this print to return to line
 printf("\n");
-
-//this for is to print the table border + the contenu of the table
-    for (i=0;i<9;i++){
-        //this if is to avoid printig an additional row
-      printf(" ");
-      for(k=0;k<19;k++){ printf("%c",borderRows[i][k]);}
-       printf("\n");
-        //this print is to print the right side indexation
-        printf("%d%c",i+1,borderColomns[i][j]);
-            //this boucle is made to display the inside of the table
-            for (j=0;j<9;j++){
-                AffichePawn(tab,i,j);
-            }
-    //this if is to return to the line after every row
-    printf("\n");
     }
-    //this if is to print the bottom side of the table (with assci code)
-    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",200,205,202,205,202,205,202,205,202,205,202,205,202,205,202,205,202,205,188);
+}
+//this to initialize the game (it will reset the number of bars and the first place of the pawns
+InitializePlayer(int tab[19][19],player *p1,player *p2){
+//init player 1
+p1->id=1;
+p1->col =1;
+p1->row =9;
+p1->bars_numbers=10;
+p1->turn=0;
+//init player 2
+p2->id=2;
+p2->col =17;
+p2->row =9;
+p2->bars_numbers=10;
+p2->turn=1;
 
-    //this print is to make space for the indexation so it will align with table rows
-    printf("  ");
-    //this for is for the indexation of the bottom row
-    for (i=0;i<9;i++){
-        printf("%d ",i+1);
-    }
-
+//init places of pawns in the board
+tab[p1->col][p1->row]=p1->id;
+tab[p2->col][p2->row]=p2->id;
 }
 
-void CheckKey(void) {
+
+
+
+
+//this function is to move pawns
+//this function is to move pawns
+MovePawns(int tab[19][19],player *p1,player *p2){
 int key;
+//khbit() is a predefini function
 if (kbhit()) {
     key=getch();
     if (key == 224) {
         do {
             key=getch();
         } while(key==224);
-        switch (key) {
-            case 72:
-                printf("up");
-                break;
-            case 75:
-                printf("left");
-                break;
-            case 77:
-                printf("right");
-                break;
-            case 80:
-                printf("down");
-                break;
-        }
+        if(key==72 && tab[p1->col-1][p1->row]!=4){
+            tab[p1->col][p1->row]=0;
+            tab[p1->col-2][p1->row]=p1->id;
+            p1->col=p1->col-2;
+
+            p1->turn=0;
+            p2->turn=1;
+            //printf("up");
+        }else if(key==75 && tab[p1->col][p1->row-1]!=4){
+            tab[p1->col][p1->row]=0;
+            tab[p1->col][p1->row-2]=p1->id;
+            p1->row=p1->row-2;
+            p1->turn=0;
+            p2->turn=1;
+            //printf("left");
+        }else if(key==77 && tab[p1->col][p1->row+1]!=4){
+            tab[p1->col][p1->row]=0;
+            tab[p1->col][p1->row+2]=p1->id;
+            p1->row=p1->row+2;
+            p1->turn=0;
+            p2->turn=1;
+            //printf("right");
+        }else if(key==80 && tab[p1->col+1][p1->row]!=4){
+            tab[p1->col][p1->row]=0;
+            tab[p1->col+2][p1->row]=p1->id;
+            p1->col=p1->col+2;
+            p1->turn=0;
+            p2->turn=1;
+            //printf("down");
+        }else printf("can t move pawn to a blockked path");
+      }
     }
-    printf("%d\n",key);
+system("clear");
+
 }
-}
+
+
+
+
 
 int main(){
-int board[9][9];
-int borderRows[19][19];
-int borderColomns[10][10];
+//declarations
+int board[19][19];
+player player1;
+player player2;
+//testing
+int m=1;
+int i2=17,k2=9;
+int i=1,k=9;
+//tab[1][9]=p1.id;
+//tab[17][9]=p2.id;
+//initiating the board with the 0,3 and 4
 InitBoard(board);
-board[0][4]=1;
-board[8][4]=2;
-
-int i=8,j,k=4;
-
-//initial place of the pawns
+//initiating the player and their places in the board
+InitializePlayer(board,&player1,&player2);
 
 AfficheBoard(board);
 
-
-            while (1) {
+       while (1) {
+//this khbit() is a predefinit function
         if (kbhit()) {
-            MovePawns(board,2,&i,&k);
-            AfficheBoard(board);
+//this if to switch turns between the players
+            if(player1.turn==1){
+                MovePawns(board,&player1,&player2);
+                AfficheBoard(board);
+            }else if(player2.turn==1){
+                MovePawns(board,&player2,&player1);
+                AfficheBoard(board);
+            }
+
         }
     }
-
 
 }
