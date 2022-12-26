dbaee95f3f84b5349f9bed18ba9dbf70027d431c
ahmed-bs <medamed777@gmail.com>
2022-12-26 09:24:49 +0100

    adding the if to change colors of the boards

--- projmain.cpp
+++ projmain.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include<windows.h>
 
-//this is a new type named player
+//this is a new type named player , we can make a new player using the word "player"
 struct {
 int col;
 int row;
@@ -13,6 +13,14 @@ int turn;
 }typedef player;
 
 
+//this is to change the color of the print that come after it
+BlueCyan(){
+  printf("\033[1;36m");
+}
+//this is to change color back to black
+ResetColor(){
+  printf("\033[0m");
+}
 
 //this function to remplir a table with 0 ,3,and 4;
 //0 is the places where the pawn can move to
@@ -41,6 +49,8 @@ int i,j;
         }
     }
 }
+
+
 //like it s name said ,it will affiche
 AfficheBoard(int tab[19][19]){
 int i,j;
@@ -58,42 +68,99 @@ int i,j;
                 printf("%c",2);
             }else if(i==0 && j==0){
                 //╔
-                printf("%c",201);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",201);
+                    ResetColor();
+                }else printf("%c",201);
             }else if(i==0 && j==18){
                 //╗
-                printf("%c",187);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",187);
+                    ResetColor();
+                }else printf("%c",187);
             }else if(i==0 && j%2==0){
                 //╦
-                printf("%c",203);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",203);
+                    ResetColor();
+                }else printf("%c",203);
             }else if(i==18 && j==18){
                 //╝
-                printf("%c",188);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",188);
+                    ResetColor();
+                }else printf("%c",188);
             }else if(i==18 && j==0){
                 //╚
-                printf("%c",200);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",200);
+                    ResetColor();
+                }else printf("%c",200);
             }else if(i==18 && j%2==0){
                 //╩
-                printf("%c",202);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",202);
+                    ResetColor();
+                }else printf("%c",202);
+
             }else if(j==0 && i%2==0){
                 //╠
-                printf("%c",204);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",204);
+                    ResetColor();
+                }else printf("%c",204);
             }else if(j==18 && i%2==0){
                 // ╣
-                printf("%c",185);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",185);
+                    ResetColor();
+                }else printf("%c",185);
             }else if(i%2==1 && (tab[i][j] == 3 ||tab[i][j] == 4 )){
                 // ║
-                printf("%c",186);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",186);
+                    ResetColor();
+                }else printf("%c",186);
             }else if(i%2==0 && (tab[i][j] == 3 ||tab[i][j] == 4 ) && j%2==0){
                 //╬
-                printf("%c",206);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",206);
+                    ResetColor();
+                }else printf("%c",206);
             }else if(i%2==0){
                 //═
-                printf("%c",205);
+                //if the case is 4 then change color
+                if(tab[i][j]==4){
+                    BlueCyan();
+                    printf("%c",205);
+                    ResetColor();
+                }else printf("%c",205);
             }
         }
 printf("\n");
     }
 }
+
 //this to initialize the game (it will reset the number of bars and the first place of the pawns
 InitializePlayer(int tab[19][19],player *p1,player *p2){
 //init player 1
@@ -119,7 +186,6 @@ tab[p2->col][p2->row]=p2->id;
 
 
 //this function is to move pawns
-//this function is to move pawns
 MovePawns(int tab[19][19],player *p1,player *p2){
 int key;
 //khbit() is a predefini function
@@ -129,14 +195,20 @@ if (kbhit()) {
         do {
             key=getch();
         } while(key==224);
-        if(key==72 && tab[p1->col-1][p1->row]!=4){
-            tab[p1->col][p1->row]=0;
-            tab[p1->col-2][p1->row]=p1->id;
-            p1->col=p1->col-2;
 
+        //the asci code of the up arrow is 72 ,this if to move the pawn up             //printf("up");
+        //the case should be different to 4   ([i-1][j] is the case that above the [i][j]
+        if(key==72 && tab[p1->col-1][p1->row]!=4){
+            //this to return the case to 0 so it will show nothing
+            tab[p1->col][p1->row]=0;
+            //this to make the case that above where we re now recieve 1
+            tab[p1->col-2][p1->row]=p1->id;
+            //this to update the player cordonate
+            p1->col=p1->col-2;
+            //this is to mark the end of the turn of the current player
             p1->turn=0;
             p2->turn=1;
-            //printf("up");
+
         }else if(key==75 && tab[p1->col][p1->row-1]!=4){
             tab[p1->col][p1->row]=0;
             tab[p1->col][p1->row-2]=p1->id;
@@ -186,17 +258,20 @@ InitBoard(board);
 InitializePlayer(board,&player1,&player2);
 
 AfficheBoard(board);
-
+     printf("\n player 1 turn|| %d bars",player1.bars_numbers);
        while (1) {
 //this khbit() is a predefinit function
         if (kbhit()) {
 //this if to switch turns between the players
             if(player1.turn==1){
+
                 MovePawns(board,&player1,&player2);
                 AfficheBoard(board);
+     printf("\n player 1 turn|| %d bars",player1.bars_numbers);
             }else if(player2.turn==1){
                 MovePawns(board,&player2,&player1);
                 AfficheBoard(board);
+     printf("\n player 2 turn|| %d bars",player2.bars_numbers);
             }
 
         }
